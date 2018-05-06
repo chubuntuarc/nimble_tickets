5659d409682f0eecb8f70faa9c4bc2fc6d18bcbc
dev.jesusarc <dev.jesusarc@gmail.com>
2018-05-06 02:38:31 -0400

    View all tickets

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
 //App global vars
 include '../app_nimble/data/vars.php'; 
+//Required classes
+require_once '../app_nimble/data/class/Ticket.php';
+//Objects
+$tickets = Ticket::getAllOpens();
 //Html head
 include '../app_nimble/element/head.php'; 
 //CSS
@@ -9,6 +13,8 @@ echo '<link rel="stylesheet" href="../app_nimble/css/cards.css">';
 include_once '../app_nimble/element/navbar.php';
 //Side bar
 include_once '../app_nimble/element/sidebar.php';
+//html tags
+echo '<div class="row" style="height: 30px;margin-top: 50px;background-color: '.$main_color.';margin-bottom: -50px;"></div>';
 ?>     
 
 <div class="row" style="margin-top:55px;">
@@ -24,51 +30,61 @@ include_once '../app_nimble/element/sidebar.php';
   <div class="col s12">
     <div id="open" class="col s12">
       <div class="row" style="color:grey;font-size:11px;">
-          <div class="col s12 m4">
-              <div class="card z-depth-1">
-                <div class="card-stacked">
-                  <div class="card-content">
-                      <div class="row">
-                        <div class="col s6 left-align">
-                          <p>Folio: 223</p>
-                        </div>
-                        <div class="col s6 right-align">
-                          <p>05/05/2018 18:30</p>
-                        </div>
-                      </div>
-                      <div class="row" style="margin-bottom:-10px;">
-                        <div class="col s12 left-align">
-                          <p style="margin-top: -14px;font-size: 14px;">Jesus Arciniega</p>
-                        </div>
-                      </div> 
-                      <div class="row">
-                        <div class="col s12 left-align">
-                          <div class="chip" style="height:20px !important;font-size:10px !important;line-height: 21px !important;margin-top: 0px;margin-left: 0px;">
-                            Ventas
-                          </div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col s12 left-align">
-                          <p style="border-bottom: #e4e4e4 solid 1px;padding-bottom:5px;font-size:14px;">Hola necesito que me apoyten con un detalle que tengo</p>
-                        </div>
-                      </div> 
-                      <div class="row" style="margin-bottom:-20px;">
-                        <div class="col s12 left-align">
-                              <p>Ventas / Soporte</p>
-                          </div>
-                        </div>
-                      </div>
-                  </div>
-                  <div class="card-action center-align" style="border-top: #64b5f6 solid;">
-                    <a href="#" <?php echo 'style="color: '.$main_color.'"' ?>>Finalizar</a>
-                  </div>
-              </div>
-            </div>
-          </div>
+        <?php
+        if(!$tickets){
+          echo 'Sin tickets registrados';
+        }else{
+        foreach($tickets as $item):
+        echo '<div class="col s12 m4">';
+          echo '<div class="card z-depth-1">';
+             echo '<div class="card-stacked">';
+                echo '<div class="card-content">';
+                    echo '<div class="row">';
+                      echo '<div class="col s6 left-align">';
+                        echo '<p>Folio: '.$item['id_ticket'].'</p>';
+                      echo '</div>';
+                      echo '<div class="col s6 right-align">';
+                        echo '<p>'.$item['start_time'].'</p>';
+                      echo '</div>';
+                    echo '</div>';
+                    echo '<div class="row" style="margin-bottom:-10px;">';
+                      echo '<div class="col s12 left-align">';
+                        echo '<p style="margin-top: -14px;font-size: 14px;">'.$item['first_name'].' '.$item['last_name'].'</p>';
+                      echo '</div>';
+                    echo '</div>';
+                    echo '<div class="row">';
+                      echo '<div class="col s12 left-align">';
+                        echo '<div class="chip" style="height:20px !important;font-size:10px !important;line-height: 21px !important;margin-top: 0px;margin-left: 0px;background-color:#64b5f6;color:#FFF;">';
+                          echo $item['department_name'];
+                        echo '</div>';
+                      echo '</div>';
+                    echo '</div>';
+                    echo '<div class="row">';
+                      echo '<div class="col s12 left-align">';
+                        echo '<p style="border-bottom: #e4e4e4 solid 1px;padding-bottom:5px;font-size:14px;">'.$item['message'].'</p>';
+                      echo '</div>';
+                    echo '</div>';
+                    echo '<div class="row" style="margin-bottom:-20px;">';
+                      echo '<div class="col s12 left-align">';
+                        echo '<p>'.$item['dept'].'</p>';
+                      echo '</div>';
+                    echo '</div>';
+               echo '</div>';
+            echo '</div>';
+                echo '<div class="card-action center-align" style="border-top: #64b5f6 solid;">';
+                  echo '<a href="#" style="color: '.$main_color.'">Finalizar</a>';
+                echo '</div>';
+                echo '</div>';
+         echo '</div>';
+         endforeach;
+        }
+        ?>                                                           
+        </div>
       </div>
     </div>
-    <div id="closed" class="col s12">Test 2</div>
+    <div id="closed" class="col s12">
+      Test 2
+    </div>
   </div>
 
 <div class="fixed-action-btn">
